d6aa07f21cbea48de9a2ab49ee05a203a653d73c
pkrastev <petar.krastev25@gmail.com>
2020-07-24 19:33:18 +0300

    [ADD] Session model & repo

--- pkg/auth/model/model.go
+++ pkg/auth/model/model.go
@@ -2,9 +2,18 @@ package model
 
 import (
 	"survey-api/pkg/user/model"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type AuthUser struct {
 	Token string            `json:"token"`
 	User  *model.ClientUser `json:"user"`
+}
+
+type Session struct {
+	Id           primitive.ObjectID `bson:"_id,omitempty"`
+	UserId       primitive.ObjectID `bson:"user_id,omitempty"`
+	Token        string             `bson:"token,omitempty"`
+	LastModified primitive.DateTime `bson:"last_modified,omitempty"`
 }
---
+++ pkg/auth/repo/repo.go
@@ -0,0 +1,124 @@
+package repo
+
+import (
+	"context"
+	"survey-api/pkg/auth/model"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	sessionValiditySeconds = time.Hour * time.Duration(12)
+)
+
+type Service struct {
+	client *mongo.Client
+}
+
+func New(client *mongo.Client) (*Service, error) {
+	repo := &Service{client: client}
+	err := repo.createUserIndexes()
+	if err != nil {
+		return nil, err
+	}
+
+	return repo, nil
+}
+
+func (s *Service) InsertOne(session *model.Session) (*model.Session, error) {
+	session.Id = primitive.NewObjectID()
+	session.LastModified = primitive.NewDateTimeFromTime(time.Now())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	_, err := s.sessionCollection().InsertOne(ctx, session)
+	defer cancel()
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func (s *Service) FindById(sessionIdString string) (*model.Session, error) {
+	sessionId, err := primitive.ObjectIDFromHex(sessionIdString)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.FindOne(&model.Session{Id: sessionId})
+}
+
+func (s *Service) FindOne(sessionFilter *model.Session) (*model.Session, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	result := s.sessionCollection().FindOne(ctx, sessionFilter)
+	defer cancel()
+	err := result.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	var session *model.Session
+	err = result.Decode(&session)
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func (s *Service) ReplaceOne(session *model.Session) (*model.Session, error) {
+	session.LastModified = primitive.NewDateTimeFromTime(time.Now())
+	sessionFilter := &model.Session{Id: session.Id}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	_, err := s.sessionCollection().ReplaceOne(ctx, sessionFilter, session)
+	defer cancel()
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func (s *Service) DeleteOne(session *model.Session) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	_, err := s.sessionCollection().DeleteOne(ctx, session)
+	defer cancel()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) sessionCollection() *mongo.Collection {
+	return s.client.Database("survey").Collection("session")
+}
+
+func (s *Service) createUserIndexes() error {
+	collection := s.sessionCollection()
+	indexes := []mongo.IndexModel{
+		{
+			Keys: bson.M{"last_modified": 1},
+			Options: options.Index().SetExpireAfterSeconds(
+				int32(sessionValiditySeconds.Seconds()),
+			),
+		},
+		{
+			Keys: bson.M{"token": "text"},
+		},
+	}
+
+	context, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	_, err := collection.Indexes().CreateMany(context, indexes)
+	defer cancel()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
