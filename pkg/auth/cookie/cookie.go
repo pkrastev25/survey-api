c0dd882156ebc595663dfdc5d73889a4e6e6131c
pkrastev <petar.krastev25@gmail.com>
2020-07-24 19:30:07 +0300

    [ADD] Cookie & token services

---
+++ pkg/auth/cookie/cookie.go
@@ -0,0 +1,88 @@
+package cookie
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"time"
+
+	sessionmodel "survey-api/pkg/auth/model"
+
+	"github.com/gorilla/securecookie"
+)
+
+const (
+	cookieName          = "survey-session"
+	cookieValidityHours = time.Hour * time.Duration(12)
+	cookiePath          = "/token/refresh"
+)
+
+type Service struct {
+}
+
+type cookieStore struct {
+	SessionId string `json:"session_id"`
+}
+
+func New() *Service {
+	return &Service{}
+}
+
+func (s *Service) ParseSessionCookie(r *http.Request) (*http.Cookie, error) {
+	return r.Cookie(cookieName)
+}
+
+func (s *Service) GenerateSessionCookie(session *sessionmodel.Session) (*http.Cookie, error) {
+	sessionKey := os.Getenv("SESSION_KEY")
+	if len(sessionKey) == 0 {
+		return nil, errors.New("SESSION_KEY is not set")
+	}
+
+	secureCookie := securecookie.New([]byte(sessionKey), nil)
+	cookieStore := &cookieStore{SessionId: session.Id.Hex()}
+	encodedValue, err := secureCookie.Encode(cookieName, cookieStore)
+	if err != nil {
+		return nil, err
+	}
+
+	maxAge := int(time.Now().Add(cookieValidityHours).Unix())
+	cookie := s.generateCookie(encodedValue, maxAge)
+	return cookie, nil
+}
+
+func (s *Service) ValidateSessionCookie(sessionCookie *http.Cookie) (string, error) {
+	sessionKey := os.Getenv("SESSION_KEY")
+	if len(sessionKey) == 0 {
+		return "", errors.New("SESSION_KEY is not set")
+	}
+
+	secureCookie := securecookie.New([]byte(sessionKey), nil)
+	cookieStore := &cookieStore{}
+	err := secureCookie.Decode(cookieName, sessionCookie.Value, &cookieStore)
+	if err != nil {
+		return "", err
+	}
+
+	return cookieStore.SessionId, nil
+}
+
+func (s *Service) GenerateExpiredCookie() *http.Cookie {
+	return s.generateCookie("", -1)
+}
+
+func (s *Service) generateCookie(value string, maxAge int) *http.Cookie {
+	cookie := &http.Cookie{
+		Name:     cookieName,
+		Path:     cookiePath,
+		MaxAge:   maxAge,
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+	}
+
+	if len(value) != 0 {
+		cookie.Value = value
+	}
+
+	return cookie
+}
---
+++ pkg/auth/token/token.go
@@ -0,0 +1,86 @@
+package token
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const (
+	jwtHeader            = "Authorization"
+	jwtHeaderValue       = "Bearer "
+	jwtTokenValidityMins = time.Minute * time.Duration(10)
+)
+
+type Service struct {
+}
+
+func New() *Service {
+	return &Service{}
+}
+
+func (s *Service) ParseJwtToken(r *http.Request) (string, error) {
+	parsedToken := strings.Split(r.Header.Get(jwtHeader), jwtHeaderValue)
+	if len(parsedToken) != 2 {
+		return "", errors.New("Malformed token")
+	}
+
+	tokenString := strings.TrimSpace(parsedToken[1])
+	if len(tokenString) == 0 {
+		return "", errors.New("Missing token")
+	}
+
+	return tokenString, nil
+}
+
+func (s *Service) GenerateJwtToken(userId string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Subject:   userId,
+		ExpiresAt: time.Now().Add(jwtTokenValidityMins).Unix(),
+	})
+	jwtKey := os.Getenv("JWT_KEY")
+	if len(jwtKey) == 0 {
+		return "", errors.New("JWT_KEY is not set")
+	}
+
+	tokenString, err := token.SignedString([]byte(jwtKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func (s *Service) ValidateJwtToken(tokenString string) (string, error) {
+	jwtKey := os.Getenv("JWT_KEY")
+	if len(jwtKey) == 0 {
+		return "", errors.New("JWT_KEY is not set")
+	}
+
+	claims := &jwt.StandardClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Unexpected signing method: " + token.Method.Alg())
+		}
+
+		return []byte(jwtKey), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", errors.New("Invalid token")
+	}
+
+	if len(claims.Subject) == 0 {
+		return "", errors.New("Malformed token")
+	}
+
+	return claims.Subject, nil
+}
